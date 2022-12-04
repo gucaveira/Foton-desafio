55b1f3c8f96b129ce27599058e8bd6e7affbd36c
gustavo pereira da rocha <pereiragustavo215@gmail.com>
2022-12-04 10:22:01 -0300

    criando layout para detalhes

--- app/src/main/java/com/gustavo/foton/desafio/marvel/presentation/detail/DetailFragment.kt
+++ app/src/main/java/com/gustavo/foton/desafio/marvel/presentation/detail/DetailFragment.kt
@@ -1,19 +1,63 @@
 package com.gustavo.foton.desafio.marvel.presentation.detail
 
 import android.os.Bundle
+import android.transition.TransitionInflater
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.gustavo.foton.desafio.marvel.R
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.gustavo.foton.desafio.marvel.databinding.FragmentDetailBinding
+import com.gustavo.foton.desafio.marvel.framework.imageLoader.ImageLoader
+import javax.inject.Inject
 
 class DetailFragment : Fragment() {
+
+    private val viewModel: DetailViewModel by viewModels()
+
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
+
+    @Inject
+    lateinit var imageLoader: ImageLoader
+
+    private val args by navArgs<DetailFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    ) = FragmentDetailBinding.inflate(inflater, container, false).apply {
+        _binding = this
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val detailViewArg = args.detailViewArg
+        binding.imageCharacter.run {
+            transitionName = detailViewArg.name
+            imageLoader.load(this, detailViewArg.imageUrl)
+        }
+
+        setSharedElementTransitionOnEnter()
+    }
+
+    private fun setSharedElementTransitionOnEnter() {
+        TransitionInflater.from(requireContext()).inflateTransition(android.R.transition.move)
+            .apply {
+                sharedElementEnterTransition = true
+            }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        private const val FLIPPER_CHILD_POSITION_LOADING = 0
+        private const val FLIPPER_CHILD_POSITION_DETAIL = 1
+        private const val FLIPPER_CHILD_POSITION_ERROR = 2
+        private const val FLIPPER_CHILD_POSITION_EMPTY = 3
     }
 }
---
+++ app/src/main/java/com/gustavo/foton/desafio/marvel/presentation/detail/DetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.gustavo.foton.desafio.marvel.presentation.detail
+
+import androidx.lifecycle.ViewModel
+
+class DetailViewModel : ViewModel() {
+
+}
